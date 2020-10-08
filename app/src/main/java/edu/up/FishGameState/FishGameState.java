e25c491dab4a29cb9fccff471f25b6d1dcf4a662
Kyle Sanchez <sancheky22@up.edu>
2020-10-08 15:11:26 -0700

    Added a few of the methods to the GameState class

---
+++ app/src/main/java/edu/up/FishGameState/FishGameState.java
@@ -0,0 +1,28 @@
+package edu.up.FishGameState;
+
+/**
+ * @author Kyle Sanchez
+ * @author Ryan Enslow
+ * @author Carina Pineda
+ * @author Linda Nguyen
+ * */
+
+
+public class FishGameState {
+
+
+    // Default constructor
+    public FishGameState(){
+
+    }
+
+    // copy constructor
+    public FishGameState(FishGameState original){
+
+    }
+
+    @Override
+    public String toString(){
+        return "";
+    }
+}
